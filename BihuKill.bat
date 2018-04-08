a39602a678263fd5fcc54b40a856d948e72a9aab
jackielg <jackie.lg@hotmail.com>
2018-04-09 07:52:21 +0800

    update

---
+++ BihuKill.bat
@@ -0,0 +1,13 @@
+@echo off
+
+
+
+@TASKKILL>nul /FI "IMAGENAME eq python.exe"  /F /T
+@TASKLIST>nul /FI "WINDOWTITLE eq BihuCheck-*" /FI "IMAGENAME eq cmd.exe" ||exit
+@TASKKILL>nul /FI "WINDOWTITLE eq BihuCheck-*" /FI "IMAGENAME eq cmd.exe"  /F /T 
+
+echo ****** %date% %time%
+echo ****** kill process success.
+
+
+
